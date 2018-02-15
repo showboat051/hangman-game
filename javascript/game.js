cb3dcd483d144fa444f0923698f97bbf4c6ca614
showboat051 <rashad.davis87@yahoo.com>
2018-02-14 18:31:22 -0600

    Added puzzleBank and functions for picking and showing puzzles

--- javascript/game.js
+++ javascript/game.js
@@ -1,11 +1,14 @@
+// The arrays with puzzle display and intended right answer
 Action = {};
 Action.puzzle = [];
 Action.underLine = [];
 
+// Chances towards failure,and puzzle bank 
 Action.chances = 6;
 Action.puzzleInBank = puzzle.length;
-Action.underLine = puzzle.length;
+//Action.underLine = puzzle.length;
 
+// The puzzle displayed on the screen
 Action.puzzle = "Test";
 Action.underLine = "";
 
@@ -23,5 +26,23 @@ Action.setUnderLine = function showWordFunction() {
         Action.underLine[i] = "_";
     }
     Action.underLine = Action.underLine.join("");
+    document.getElementById("PUZZLE").innerHTML = Action.underLine;
+    document.getElementById("numLetters").innerHTML = Action.Word.length;
 
+}
+
+Action.UpdateLetter = function pickedLettersfunction(letter){
+    Action.changes = 0;
+    for (i = 0; i < Action.puzzle.length; i++){
+        Action.puzzle[i] = Action.puzzle.charAt(i);
+        if(Action.puzzle.charAt(i) == letter){
+            Action.underLine[i] == letter;
+            Action.changes += 1;
+        }
+    }
+
+    if(Action.changes < 1){
+        Action.chances -=1;
+        document.getElementById("chances").innerHTML = Action.chances;
+    }
 }
